e50b1a505ea652ea1f922c0db54681841c45272d
Advait Mangesh Deshpande <deshpandeadvait88@gmail.com>
2018-12-17 18:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '/')));
 //var SDK = require('blocksdk');
 //var sdk = new SDK();
 //console.log(sdk);
